053eb2ba44678097acfb5b2a5f885931ba1b8e9d
gcastro98 <36447323+gcastro98@users.noreply.github.com>
2021-03-29 23:08:12 +0200

    Carga de bases de datos y calculo de duracion (no funciona el paso del dataframe a la funcion calculo_duracion(df)

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import mysql.connector;
+import numpy as np
+import pandas as pd;
 
 
-# Press the green button in the gutter to run the script.
+def get_connection():
+    db_connection = mysql.connector.connect(
+        host="127.0.0.1",
+        port="3306",
+        user="root",
+        passwd="",
+        auth_plugin='mysql_native_password',
+        database='db_practica'
+    )
+    return db_connection
+
+
+def load_to_dataframe(db_conn):
+    df = pd.read_sql("SELECT * FROM table_practica", con=db_conn)
+    pd.set_option('display.max_columns', None)
+    desired_width = 320
+    pd.set_option('display.width', desired_width)
+
+
+
+def calculo_duracion(df):
+    duracion = df.get("duration")
+    min, season = []
+    for o in duracion:
+        aux = int(o.split(" ")[0])
+        if " min" in o:
+            min.append(aux)
+        if " Seasons" in o:
+            season.append(aux)
+
+    print("\nPeliculas:")
+    print(f'Valor medio de duracion de las peliculas: {np.average(min):,.2f} minutos')
+    print(f'Valor de la desviacion tipica de la duracion de las peliculas: {np.std(min):,.2f}')
+    print("\nTVShows:")
+    print(f'Valor medio de duracion de las TVSHOWS: {np.average(season):,.2f} temporadas')
+    print(f'Valor de la desviacion tipica de la duracion de las TVSHOWS: {np.std(season):,.2f}')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-print("Hola")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    db_connector = get_connection()
+    df = load_to_dataframe(db_connector)
+    calculo_duracion(df)
